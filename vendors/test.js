d4c203f337fc379d4bfb39c257b57b4db21120cd
paz198787 <patrick.sjoberg@student.kyh.se>
2018-11-08 14:26:34 +0100

    index example save function too

--- vendors/test.js
+++ vendors/test.js
@@ -29,4 +29,35 @@ document.getElementById('editor').innerHTML = localStorage['text'] || 'This text
 
  setInterval(function() {
       localStorage['text'] = document.getElementById('editor').innerHTML;
- }, 10);
+
+ }, 100);
+ /*
+var index = {
+    nextFreeIndex: 4,
+    lastOpened: 'text0',
+    notes: [
+        {key: 'text0', 
+        title: 'Smarta tips o tricks'
+        modified: ?? Date-objekt?? sträng??,
+        tags: ['frontend', 'topsecret']
+    },
+        {key: 'text2', 
+        tags: ['handlingslista']
+},
+        {key: 'text3', 
+        tags: ['hair']
+    }
+]
+var index = { //first time
+    nextFreeIndex: 0,
+    lastOpened: '',
+    notes: []
+}
+
+var text0 = {
+    content: '<html>blablablbla</html>'
+
+}
+
+ */
+

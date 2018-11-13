450f7f6d297ac2d4e0395d5997b50c2c82206e08
bayev <azamat.azhibayev@student.kyh.se>
2018-11-13 14:14:47 +0100

    pushing to my branch

---
+++ test.js
@@ -0,0 +1,91 @@
+
+// document.getElementById("editor").addEventListener("submit",
+//         function(event) {
+//             console.log(Hello);
+            
+//             event.preventDefault();
+//             var content = document.getElementById('editor').innerHTML;
+//             console.log( "this is it");
+//             var numberOfListItems = listData.length;
+//             console.log(musicfiles, "this is it");
+//         });
+    
+
+
+
+// function check_web_storage_support() {
+//     if(typeof(Storage) !== "undefined") {
+//         return(true);
+//     }
+//     else {
+//         alert("Web storage unsupported!");
+//         return(false);
+//     }
+// }
+
+
+// function save() {
+//     if(check_web_storage_support() == true) {
+//         var area = document.getElementById("editor");
+//         console.log(area.textContent);
+//         if(area.textContent != '') {
+//             localStorage.setItem("note", area.textContent);
+//         }
+//         else {
+//             alert("Nothing to save");
+//         }
+//     }
+// }
+
+// SAVE AND LOAD BUTTON CODED
+document.getElementById('editor').innerHTML = localStorage['text'] || 'This text is automatically saved every second :) ';
+document.getElementById("submit").onclick = function () { 
+    localStorage['text'] = document.getElementById('editor').innerHTML;
+}
+
+
+// document.getElementById("submit").onclick = function() {
+//     var li = document.createElement("list");
+//     var inputValue = document.getElementById("editor").innerHTML;
+//     var t = document.createTextNode(inputValue);
+//     li.appendChild(t);
+// }
+
+// document.getElementById("submit").onclick = function() {
+//     var objTo = document.getElementById("myDiv");
+//     var divNew = document.createElement("div");
+//     divNew.innerHTML = document.getElementById("editor").textContent;
+//     objTo.appendChild(divNew);
+// }
+
+
+/*
+var index = {
+    nextFreeIndex: 4,
+    lastOpened: 'text0',
+    notes: [
+        {key: 'text0', 
+        title: 'Smarta tips o tricks'
+        modified: ?? Date-objekt?? sträng??,
+        tags: ['frontend', 'topsecret']
+    },
+        {key: 'text2', 
+        tags: ['handlingslista']
+},
+        {key: 'text3', 
+        tags: ['hair']
+    }
+]
+var index = { //first time
+    nextFreeIndex: 0,
+    lastOpened: '',
+    notes: []
+}
+
+var text0 = {
+    content: '<html>blablablbla</html>'
+
+}
+
+ */
+

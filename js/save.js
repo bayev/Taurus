f18355658c8f9d926e67e77d2cfa1f8e5c4d8917
eddyking1 <edward.kumerius@hotmail.com>
2018-11-15 12:49:19 +0100

    Fixed issues with quill rich text documents tool bar did not work but now it does, problem roots down to script tag needed in other place than bottom of body

--- js/save.js
+++ js/save.js
@@ -1,5 +1,66 @@
-var modals document.getElementsByClassName("quillText")[0];
 
-    document.getElementById('editor').innerHTML = localStorage['text'] || 'This text is automatically saved every second :) ';
+window.onload = function (){
+  var store = document.getElementById('editor').innerHTML = localStorage['text'] || 'This text is automatically saved every second :) ';
+  var fix = document.getElementById('editor');
+  fix.setAttribute("contenteditable", "true");
+
+}
+
+
+var modals = document.getElementsByClassName("quillText")[0];
+
+document.getElementById('editor').innerHTML = localStorage['text'];
+
     document.getElementById("submit").onclick = function () {
-        localStorage['text'] = document.getElementById('editor').innerHTML;   }
+        localStorage['text'] = document.getElementById('editor').innerHTML;
+}
+
+
+
+var Delta = Quill.import('delta');
+var quill = new Quill('#editor', {
+  modules: {
+    toolbar: false
+  },
+  placeholder: 'Compose an epic...',
+  theme: 'snow',
+});
+
+// Store accumulated changes
+var change = new Delta();
+quill.on('text-change', function(delta) {
+  change = change.compose(delta);
+});
+
+
+// Save periodically
+setInterval(function() {
+  if (change.length() > 0) {
+    console.log('Saving changes', change);
+
+    //var test = JSON.stringify(quill.getContents());
+    //quill.setContents(JSON.parse(test));
+    console.log(user);
+
+    // JSON.stringify(quill.getContents())
+    /*
+    Send partial changes
+    $.post('/your-endpoint', {
+      partial: JSON.stringify(change)
+    });
+
+    Send entire document
+    $.post('/your-endpoint', {
+      doc: JSON.stringify(quill.getContents())
+    });
+    */
+    change = new Delta();
+  }
+}, 5*1000);
+
+// Check for unsaved data
+window.onbeforeunload = function() {
+  if (change.length() > 0) {
+    return 'There are unsaved changes. Are you sure you want to leave?';
+  }
+}

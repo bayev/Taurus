dae2fd2943c357aed343a99adcf14ed104882431
paz198787 <patrick.sjoberg@student.kyh.se>
2018-12-12 02:35:14 +0100

    mobile view and nav toggle fixed

--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,10 @@
 document.getElementById('buttonEx').addEventListener('click', toggle);
+document.getElementById('hiddenLogo').addEventListener('click', toggle2);
+
 
 var clicks1 = 0;
+var clicks2 = 0;
+
 
 function toggle() {
   let book = document.getElementsByTagName("i")[2];
@@ -17,3 +21,25 @@ function toggle() {
     book.style.color = "black";
 };
 };
+
+function toggle2() {
+    let book = document.getElementsByTagName("i")[2];
+  
+    clicks2 += 1;
+  
+    if (clicks2 % 2) {
+      document.getElementsByTagName('nav')[0].classList.add("navShow");
+      document.getElementById('notes').classList.add("moved");
+      book.style.color = "gold";
+      clicks1 += 1;
+
+
+  
+  } else {
+      document.getElementsByTagName('nav')[0].classList.remove("navShow");
+      document.getElementById('notes').classList.remove("moved");
+      book.style.color = "black";
+
+  };
+  };
+  

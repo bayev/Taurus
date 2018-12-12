0eff06079a0771a76cf327e466aab5eae5000a73
paz198787 <patrick.sjoberg@student.kyh.se>
2018-12-12 21:35:04 +0100

    edward changes

--- js/slider.js
+++ js/slider.js
@@ -3,11 +3,19 @@ var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,7000);
 
 function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
+    goToSlide(currentSlide+1);
 }
 
+function previousSlide() {
+    goToSlide(currentSlide-1);
+}
+
+function goToSlide(n) {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+ 
 
 var playing = true;
 var pauseButton = document.getElementById('pause');
